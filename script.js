6bfda8a158d6586985fab68a460b05d6067066b9
Ingo Davila <ingodavila99@gmail.com>
2021-10-12 10:56:03 +0800

    changed misleading details

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function compute_result()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal*rate*years /100;
+    var interest = (principal*rate*years) /100;
     var year = new Date().getFullYear() + parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year "+year+"\<br\>";
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive a profit amount of <mark>"+interest+"</mark>,\<br\>in the year "+year+"\<br\>";
 }
 
 function updateRate()
